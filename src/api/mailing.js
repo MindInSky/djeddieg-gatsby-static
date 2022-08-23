b24c52853ebf10841aaddb4368a0235b8d1d91b0
Kevin Muniz <kevin.a.muniz@gmail.com>
2022-08-23 13:48:12 -0500

    Added quick 404 and fixed mailing issue

--- src/api/mailing.js
+++ src/api/mailing.js
@@ -3,9 +3,9 @@
 // // javascript
 // const sgMail = require('@sendgrid/mail')
 
-// export default async function handler ( req, res ) {
+export default async function handler ( req, res ) {
 
-// 	console.log(`🚀 ~ file: mailing.js ~ line 6 ~ newFunction ~ req`, req)
+	console.log(`🚀 ~ file: mailing.js ~ line 6 ~ newFunction ~ req`, req)
 
 // 	const response = {
 // 		body: '',
@@ -39,58 +39,58 @@
 // 		})
 
 
-// 	return res
+	return res
 
+}
+
+// const sendgrid = require("@sendgrid/mail")
+// //Your API Key from Sendgrid
+// sendgrid.setApiKey(process.env.GATSBY_SENDGRID_KEY)
+// const message = {
+//   //Your authorized email from SendGrid
+//   from: process.env.GATSBY_SENDGRID_FROM,
+// 	to: process.env.GATSBY_SENDGRID_TO, // Change to your recipient
 // }
 
-const sendgrid = require("@sendgrid/mail")
-//Your API Key from Sendgrid
-sendgrid.setApiKey(process.env.GATSBY_SENDGRID_KEY)
-const message = {
-  //Your authorized email from SendGrid
-  from: process.env.GATSBY_SENDGRID_FROM,
-	to: process.env.GATSBY_SENDGRID_TO, // Change to your recipient
-}
+// const handler = (req, res) => {
 
-const handler = (req, res) => {
+// 	let response = {
+// 		status :'',
+// 		message : ''
+// 	}
+//   try {
+//     // if (req.method !== "POST") {
+//     //   res.json({ message: "Try a POST!" })
+//     // }
 
-	let response = {
-		status :'',
-		message : ''
-	}
-  try {
-    // if (req.method !== "POST") {
-    //   res.json({ message: "Try a POST!" })
-    // }
+//     if (req.body) {
+// 			message.subject = 'From the Contact Form'
+// 			message.text = 'and easy to do anywhere even with Node.js'
+// 			message.html = '<strong>and easy to do anywhere, even with Node.js</strong>'
+//     }
 
-    if (req.body) {
-			message.subject = 'From the Contact Form'
-			message.text = 'and easy to do anywhere even with Node.js'
-			message.html = '<strong>and easy to do anywhere, even with Node.js</strong>'
-    }
+//     return sendgrid.send(message).then(
+//       () => {
+//         res.status(200).json({
+//           message: "I will send email",
+//         })
+//       },
+//       error => {
+//         console.error(error)
+//         if (error.response) {
+//           return res.status(500).json({
+//             error: error.response,
+//           })
+//         }
+//       }
+//     )
+//   } catch (err) {
+//     console.log(err)
+// 		response.status = 500
+// 		response.message = { message: "There was an error", error: err }
 
-    return sendgrid.send(message).then(
-      () => {
-        res.status(200).json({
-          message: "I will send email",
-        })
-      },
-      error => {
-        console.error(error)
-        if (error.response) {
-          return res.status(500).json({
-            error: error.response,
-          })
-        }
-      }
-    )
-  } catch (err) {
-    console.log(err)
-		response.status = 500
-		response.message = { message: "There was an error", error: err }
+//     return response
+//   }
+// }
 
-    return response
-  }
-}
-
-module.exports = handler
+// module.exports = handler
